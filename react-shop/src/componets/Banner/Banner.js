9eeef48692d106d58da8d8971482b89280080317
aibacetat <aaron.ibaceta@hotmail.com>
2023-03-21 19:11:27 -0300

    actualizacion router

---
+++ react-shop/src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>Tienda de Accesorios Computacionales</h1>
+        <p>Tenemos todos los accesorios sino lo Inventamos!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
